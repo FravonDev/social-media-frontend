146a0fec42f073a559ce432e61d3574c2a1736b3
FravonDev <flvsadr@gmail.com>
2023-12-06 20:02:41 -0300

    feat: add react router dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 
 import './App.css'
 import { AppProvider } from './context/AppProvider'
+import { RouterComponent } from './routes'
 
 
 function App() {
 
   return (
     <AppProvider>
-      <h1 className="text-3xl text-slate-400 font-bold underline">
-        Let's start it!
-      </h1>
+      <RouterComponent />
     </AppProvider>
 
   )
