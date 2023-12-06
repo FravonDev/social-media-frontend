bf3d104d84ed3b25597ef6354e3bafba1e9e0c47
FravonDev <flvsadr@gmail.com>
2023-12-06 19:22:59 -0300

    feat: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 
 import './App.css'
+import { AppProvider } from './context/AppProvider'
+
 
 function App() {
 
   return (
-    <>
+    <AppProvider>
       <h1 className="text-3xl text-slate-400 font-bold underline">
         Let's start it!
       </h1>
-    </>
+    </AppProvider>
+
   )
 }
 
